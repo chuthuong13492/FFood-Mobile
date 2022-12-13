112630589d2e4c326d827e6bb98f0f9f1a239fa6
Smjle <denwingsa01884@gmail.com>
2022-12-13 13:42:55 +0700

    feat create UserDTO firebaseAuth

--- lib/Constant/route_constraint.dart
+++ lib/Constant/route_constraint.dart
@@ -3,4 +3,6 @@ class RoutHandler {
   static const String HOME = "/login";
   static const String NAV = "/nav";
   static const String ONBOARD = "/onboard";
+  static const String LOADING = "/loading";
+  static const String PRODUCT_FILTER_LIST = "PRODUCT_FILTER_LIST";
 }
