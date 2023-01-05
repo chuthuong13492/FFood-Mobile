bea9bc6c3b7584cabb59ce7aed804015f88a9bcc
Kien Nguyen <78245295+chuthuong13492@users.noreply.github.com>
2023-01-06 03:23:56 +0700

    login viewModel

--- lib/View/login.dart
+++ lib/View/login.dart
@@ -1,3 +1,4 @@
+import 'package:ffood/ViewModel/login_viewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -14,8 +15,25 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return ScopedModel(
-      model: model,
-      child: Scaffold(),
+      model: LoginViewModel(),
+      child: Scaffold(
+        body: getBody(),
+      ),
+    );
+  }
+
+  getBody() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.fromLTRB(0, 150, 0, 150),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Center(),
+        ],
+      ),
     );
   }
 }
